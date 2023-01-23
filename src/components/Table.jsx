6a6844ca8320d47d3f81e45d0d9124f589ec42c5
Vitor Wagner <vitormwagner@gmail.com>
2023-01-23 17:07:37 -0300

    Added numeric filters

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,27 +8,56 @@ const headers = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Clima
 function Table() {
   const [planets, setPlanets] = useState([]);
   const [nameQuery, setNameQuery] = useState('');
+  const [filters, setFilters] = useState(['population', 'orbital_period',
+    'diameter', 'rotation_period', 'surface_water']);
+  const [activeFilters, setActiveFilters] = useState([]);
 
   const getPlanets = async () => {
     const planetsList = await fetchPlanets();
     setPlanets(planetsList);
-    console.log(planetsList);
   };
 
   useEffect(() => {
     getPlanets();
   }, []);
 
-  // const nameFilter = () => planets.filter((planet) => planet
-  //   .toLowerCase().includes(nameQuery.toLowerCase()));
+  const addFilter = (e) => {
+    e.preventDefault();
+    setActiveFilters((prevState) => [...prevState, {
+      check: e.target.check.value,
+      value: e.target.value.value,
+      column: e.target.column.value,
+    }]);
+    setFilters((prevState) => prevState.filter((column) => column
+    !== e.target.column.value));
+  };
+
+  // https://www.freecodecamp.org/news/how-to-convert-a-string-to-a-number-in-javascript/
+
+  const numericCheck = ({ check, value, column }, data) => {
+    if (data[column] === 'unknown') return false;
+    switch (check) {
+    case 'maior que':
+      return +data[column] > +value;
+    case 'menor que':
+      return +data[column] < +value;
+    case 'igual a':
+      return +data[column] === +value;
+    default:
+      return true;
+    }
+  };
 
   // https://stackoverflow.com/questions/57358605/multiple-filters-in-react
 
-  const filterPlanet = (data) => {
-    // console.log(data.name);
-    if (nameQuery && !data.name.toLowerCase().includes(nameQuery.toLowerCase())) return false;
-    return true;
-  };
+  const filterPlanet = (data) => (data.name.toLowerCase()
+    .includes(nameQuery.toLowerCase())
+    && (activeFilters[0] ? numericCheck(activeFilters[0], data) : true)
+    && (activeFilters[1] ? numericCheck(activeFilters[1], data) : true)
+    && (activeFilters[2] ? numericCheck(activeFilters[2], data) : true)
+    && (activeFilters[3] ? numericCheck(activeFilters[3], data) : true)
+    && (activeFilters[4] ? numericCheck(activeFilters[4], data) : true)
+  );
 
   const filteredPlanets = planets.filter(filterPlanet);
 
@@ -40,6 +69,33 @@ function Table() {
         type="search"
         data-testid="name-filter"
       />
+      <form onSubmit={ addFilter }>
+        <select name="column" id="" data-testid="column-filter">
+          {filters.map((item) => <option key={ item }>{item}</option>)}
+        </select>
+        <select name="check" id="" data-testid="comparison-filter">
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          name="value"
+          id=""
+          data-testid="value-filter"
+          defaultValue={ 0 }
+        />
+        <button type="submit" data-testid="button-filter">Adicionar Filtro</button>
+      </form>
+      <div>
+        {activeFilters.map((filter) => (
+          <div key={ filter.column }>
+            <span>{filter.column}</span>
+            <span>{filter.check}</span>
+            <span>{filter.value}</span>
+          </div>
+        ))}
+      </div>
       <table>
         <caption>Planets</caption>
         <thead>
